c9dd6fdb6131e35cc152367bb371e2f8f9f3f033
manideep <chopperlamanideep1029@gmail.com>
2025-01-31 13:17:51 +0530

    Api level data sanitization

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const connectDB = require("./config/database")
 const UserModel = require("./models/user")
+const { ReturnDocument } = require("mongodb")
 
 const app = express()
 app.use(express.json())
@@ -24,6 +25,65 @@ app.post("/signup",async(req,res)=>{
     }
     catch(err){
         res.status(400).send("Error occured ",err.message)
+    }
+})
+
+//GET USER BY EMAIL
+app.get("/user",async(req,res)=>{
+    const email = req.body.emailId;
+    try{
+        const obj = await UserModel.findOne({emailId: email})
+        if(obj.length===0){
+            res.status(404).send("User Not Found")
+        }else{
+            res.send(obj)
+        }
+    }catch(err){
+        res.status(400).send("Unable to Retieve Data")
+    }
+})
+
+//UPDATE USER BY ID
+
+
+app.patch("/user",async(req,res)=>{
+    const id=req.body.userId
+    const data=req.body
+    console.log(data)
+    try{
+        const users = await UserModel.findByIdAndUpdate({_id: id},data,{
+            returnDocument:"after",
+            runValidators: true
+        });
+        console.log(users)
+        res.send("User Data Updated Successfully")
+    }
+    catch(err){
+        res.status(404).send("Update Failed : "+err.message)
+    }
+})
+
+//Delete USER BY ID
+
+app.delete("/user",async(req,res)=>{
+    const id=req.body.userId
+    try{
+        const users=await UserModel.findByIdAndDelete({ _id : id });
+        // const users=await UserModel.findByIdAndDelete(id)
+        res.send("User Deleted Successfully")
+    }
+    catch(err){
+        res.status(404).send("Unable to delete User")
+    }
+})
+
+//FEED API - GET / feed - get all the users from the database
+app.get("/feed", async(req,res)=>{
+    try{
+        const users = await UserModel.find({})
+        res.send(users)
+    }catch(err){
+        res.status(400).send("Unable to Retieve Data")
     }
 })
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,56 @@
 const mongoose = require("mongoose")
 
 const userSchema = mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    emailId: String,
-    password: String,
-    age: Number,
-    gender: String
+    firstName: {
+        type: String,
+        required:true,
+        minLength: 4,
+        maxLength: 50,
+    },
+    lastName: {
+        type: String,
+
+    },
+    emailId: {
+        type: String,
+        required:true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required:true,
+        minLength: 8,
+        maxLength: 15
+    },
+    age: {
+        type: Number,
+        min: 18,
+        
+    },
+    gender: {
+        type: String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender data is not valid")
+            }
+        }
+    },
+    photoUrl: {
+        type: String,
+        default: "https://www.geographyandyou.com/images/user-profile.png",
+    },
+    about: {
+        type: String,
+        default: "This is a default About of the User" 
+    },
+    skills: {
+        type:[String]
+    },
+    
+},{
+    timestamps: true
 })
 
 const UserModel = mongoose.model("User",userSchema)
